e8eecba058dfe0b480021e3fb624ec441fe8f4f2
Dmitry Sazonov <sazon@nxt.ru>
2020-02-04 12:35:17 +0300

    fixed migration

--- src/Migrations/Version20200204072504.php
+++ src/Migrations/Version20200204072504.php
@@ -26,8 +26,6 @@ final class Version20200204072504 extends AbstractMigration
         $this->addSql('CREATE TABLE PageUrl (id INT AUTO_INCREMENT NOT NULL, entity_id INT DEFAULT NULL, url VARCHAR(255) DEFAULT NULL, created DATETIME DEFAULT NULL, INDEX IDX_69819C0681257D5D (entity_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET UTF8 COLLATE `UTF8_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE Page (id INT AUTO_INCREMENT NOT NULL, meta_keywords LONGTEXT DEFAULT NULL, meta_description LONGTEXT DEFAULT NULL, created DATETIME DEFAULT NULL, updated DATETIME DEFAULT NULL, visible TINYINT(1) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET UTF8 COLLATE `UTF8_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE PageUrl ADD CONSTRAINT FK_69819C0681257D5D FOREIGN KEY (entity_id) REFERENCES Page (id)');
-        $this->addSql('ALTER TABLE users DROP confirmation_token');
-        $this->addSql('ALTER TABLE users RENAME INDEX uniq_1483a5e9e7927c74 TO UNIQ_D5428AEDE7927C74');
     }
 
     public function down(Schema $schema) : void
@@ -39,7 +37,5 @@ final class Version20200204072504 extends AbstractMigration
         $this->addSql('DROP TABLE Block');
         $this->addSql('DROP TABLE PageUrl');
         $this->addSql('DROP TABLE Page');
-        $this->addSql('ALTER TABLE Users ADD confirmation_token VARCHAR(30) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`');
-        $this->addSql('ALTER TABLE Users RENAME INDEX uniq_d5428aede7927c74 TO UNIQ_1483A5E9E7927C74');
     }
 }
