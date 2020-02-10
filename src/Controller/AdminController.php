7d4e875497740d3352cc68dd122e55da9a25885b
Dmitry Sazonov <sazon@nxt.ru>
2020-02-10 18:53:59 +0300

    fixed saving

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -11,40 +11,20 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AdminController as BaseAdminContr
  */
 class AdminController extends BaseAdminController
 {
-    /**
-     * @param $entity
-     */
     protected function persistEntity($entity)
     {
         $this->em->persist($entity);
         $this->em->flush();
     }
 
-    /**
-     * @param $entity
-     */
     protected function updateEntity($entity)
     {
         $this->em->flush();
     }
 
-    /**
-     * @param $entity
-     */
     protected function removeEntity($entity)
     {
         $this->em->remove($entity);
         $this->em->flush();
     }
-
-    /**
-     * @param $className
-     * @return mixed
-     */
-    public function repository($className)
-    {
-        return $this->getDoctrine()->getRepository($className);
-    }
-
-
 }
