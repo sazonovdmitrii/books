804b66a826dd11a19294217bc6a48f69d76b61d4
Dmitry Sazonov <sazon@nxt.ru>
2020-02-11 14:49:59 +0300

    admin stuff

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -11,35 +11,5 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AdminController as BaseAdminContr
  */
 class AdminController extends BaseAdminController
 {
-    /**
-     * @param object $entity
-     * @throws \Doctrine\ORM\ORMException
-     * @throws \Doctrine\ORM\OptimisticLockException
-     */
-    protected function persistEntity($entity)
-    {
-        $this->em->persist($entity);
-        $this->em->flush();
-    }
-
-    /**
-     * @param object $entity
-     * @throws \Doctrine\ORM\ORMException
-     * @throws \Doctrine\ORM\OptimisticLockException
-     */
-    protected function updateEntity($entity)
-    {
-        $this->em->flush();
-    }
-
-    /**
-     * @param object $entity
-     * @throws \Doctrine\ORM\ORMException
-     * @throws \Doctrine\ORM\OptimisticLockException
-     */
-    protected function removeEntity($entity)
-    {
-        $this->em->remove($entity);
-        $this->em->flush();
-    }
+    
 }
